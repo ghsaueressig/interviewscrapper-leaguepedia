5146ed6a80f77ae0da2ae20a830845f8e0630d5a
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2026-08-22 20:22:12 -0300

    Refactor TEAM_MAP for better readability

--- backend/app.py
+++ backend/app.py
@@ -13,9 +13,16 @@ app = Flask(__name__)
 CORS(app)
 
 TEAM_MAP = {
-    'pain': 'paiN Gaming', 'los': 'LOS', 'loud': 'LOUD', 'furia': 'FURIA',
-    'red': 'RED Canids', 'vks': 'Vivo Keyd Stars', 'fx': 'Fluxo W7M',
-    'fluxo': 'Fluxo W7M', 'fxw7m': 'Fluxo W7M', 'w7m': 'Fluxo W7M',
+    'pain': 'paiN Gaming',
+    'los': 'LOS',
+    'loud': 'LOUD',
+    'furia': 'FURIA',
+    'red': 'RED Canids',
+    'vks': 'Vivo Keyd Stars',
+    'fx': 'Fluxo W7M',
+    'fluxo': 'Fluxo W7M',
+    'fxw7m': 'Fluxo W7M',
+    'w7m': 'Fluxo W7M',
     'lev': 'Leviatán'
 }
 
