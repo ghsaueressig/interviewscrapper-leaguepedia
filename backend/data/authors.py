b1522bd4efe81b213e1f665cf167572524218760
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2026-08-23 15:06:09 -0300

    Create authors.py

---
+++ backend/data/authors.py
@@ -0,0 +1,10 @@
+AUTHOR_MAPPINGS = [
+    {
+        "matches": ["Corres", "Fiorini"],
+        "wiki": '[[Self:Corres|Sérgio "Corres" Fiorini]]'
+    },
+    {
+        "matches": ["Ian Teixeira"],
+        "wiki": "[[Ian Teixeira]]"
+    }
+]
