ae3d86520ed4a5bcba9887800779cd5a7825af97
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2026-08-23 15:04:00 -0300

    Add TEAM_MAP for team name mappings

---
+++ backend/data/teams.py
@@ -0,0 +1,13 @@
+TEAM_MAP = {
+    'pain': 'paiN Gaming',
+    'los': 'LOS',
+    'loud': 'LOUD',
+    'furia': 'FURIA',
+    'red': 'RED Canids',
+    'vks': 'Vivo Keyd Stars',
+    'fx': 'Fluxo W7M',
+    'fluxo': 'Fluxo W7M',
+    'fxw7m': 'Fluxo W7M',
+    'w7m': 'Fluxo W7M',
+    'lev': 'Leviatán'
+}
