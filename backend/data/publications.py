ab16186a85693851d6370b73470244b1977c2f38
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2026-08-23 15:05:11 -0300

    Add publications and video domains constants
    Define constants for publications and video domains.

---
+++ backend/data/publications.py
@@ -0,0 +1,21 @@
+PUBLICATIONS = {
+    "maisesports.com.br": "Mais Esports",
+    "sheepesports.com": "Sheep Esports",
+}
+
+
+WRITTEN_PUBLICATIONS = {
+    "maisesports.com.br",
+    "sheepesports.com",
+}
+
+
+VIDEO_DOMAINS = {
+    "youtube.com",
+    "youtu.be",
+    "youtube-nocookie.com",
+    "vimeo.com",
+    "twitch.tv",
+    "kick.com",
+    "streamable.com",
+}
