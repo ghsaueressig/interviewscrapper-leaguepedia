15eeac003e96a84bc58106fe76cbf8ea3f5066c9
Guilherme Henke Saueressig <96593677+ghsaueressig@users.noreply.github.com>
2026-08-27 22:53:12 -0300

    Fix syntax errors in authors.py
    Fixed syntax errors by adding missing commas.

--- backend/data/authors.py
+++ backend/data/authors.py
@@ -6,21 +6,21 @@ AUTHOR_MAPPINGS = [
     {
         "matches": ["Ian Teixeira"],
         "wiki": "[[Ian Teixeira]]"
-    }
+    },
     {
         "matches": ["Diniz"],
         "wiki": "[[Diniz]]"
-    }
+    },
     {
         "matches": ["Nicole Pereira"]
         "wiki": "[[Nicole Pereira]]"
-    }
+    },
     {
         "matches": ["Wladimir Neto"]
         "wiki": "[[Wladimir Neto]]"
-    }
+    },
     {
         "matches": ["João Pedro Andrada"]
         "wiki": "[[João Pedro Andrada]]"
-    }
+    },
 ]
